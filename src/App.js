8d65239d26f21abb6c0d74ec84ee1769a398709f
notik <macsly@ukr.net>
2021-01-24 22:49:18 +0200

    Основы ReactJS - 19 Работа с условными операторами

--- src/App.js
+++ src/App.js
@@ -10,19 +10,18 @@ class App extends Component {
       {name: 'Audi', year: 2016},
       {name: 'Mazda', year: 2010}
     ],
-    pageTitle: 'React components'
+    pageTitle: 'React components',
+    showCars: false
   }
 
-  changeTitleHandler = (newTitle) => {
+  toggleCarsHandler = () => {
     this.setState({
-      pageTitle: newTitle
+      showCars: !this.state.showCars
     })
   }
 
-  handleInput = (event) => {
-    this.setState({
-      pageTitle: event.target.value
-    })
+  changeTitleHandler = pageTitle => {
+    this.setState({pageTitle})
   }
 
   render() {
@@ -30,26 +29,30 @@ class App extends Component {
       textAlign: 'center'
     }
 
+    let cars = null
+
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            name={car.name}
+            year={car.year}
+            onChangeTitle={() => this.changeTitleHandler(car.name)}
+          />
+        )
+      })
+    }
+
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
 
-        <input type="text" onChange={this.handleInput} />
-
         <button
-          onClick={this.changeTitleHandler.bind(this, 'Changed!')}
-        >Change title</button>
+          onClick={this.toggleCarsHandler}
+        >Toggle cars</button>
 
-        { this.state.cars.map((car, index) => {
-          return (
-            <Car
-              key={index}
-              name={car.name}
-              year={car.year}
-              onChangeTitle={() => this.changeTitleHandler(car.name)}
-            />
-          )
-        }) }
+        { cars }
       </div>
     );
   }
