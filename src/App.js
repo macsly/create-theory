4db00198fb04eae118db39c4bb5f5bd496c5182c
notik <macsly@ukr.net>
2021-01-24 22:45:31 +0200

    Основы ReactJS - 15 Изменение State

--- src/App.js
+++ src/App.js
@@ -14,10 +14,19 @@ class App extends Component {
   }
 
   changeTitleHandler = () => {
-    console.log('Clicked')
+
+    const oldTitle = this.state.pageTitle
+
+    const newTitle = oldTitle + ' (changed)'
+
+    this.setState({
+      pageTitle: newTitle
+    })
+
   }
 
   render() {
+    console.log('Render')
     const divStyle = {
       textAlign: 'center'
     }
