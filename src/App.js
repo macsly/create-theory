76e9d651d54cf6deb7ec29d4b157dbc21093b69f
notik <macsly@ukr.net>
2021-01-24 20:46:42 +0200

    8 lesson JSX - Создание простого компонента

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Car from "./Car/Car";
 
 class App extends Component {
     render() {
@@ -12,6 +13,7 @@ class App extends Component {
                 <h1 style={{color: 'blue', fontSize: '20px'}}>Hello world!</h1>
             </div>
         );
+        <Car />
     }
 }
 
